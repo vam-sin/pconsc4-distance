4d4226e29b44a1c47e6bd7158fe0835e65afeb6e
Vamsi Nallapareddy <vamsinallapareddy@gmail.com>
2020-07-02 19:56:57 +0530

    PPV

--- src/train.py
+++ src/train.py
@@ -6,6 +6,8 @@ from model import unet
 import pickle
 import random
 import keras
+from keras.models import load_model 
+from sklearn.metrics import classification_report, confusion_matrix
 
 # GPU
 from tensorflow.compat.v1 import ConfigProto
@@ -78,8 +80,88 @@ reduce_lr = keras.callbacks.callbacks.ReduceLROnPlateau(monitor='val_accuracy', 
 callbacks_list = [reduce_lr, mcp_save]
 
 model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
-with tf.device('/cpu:0'):
-  model.fit_generator(train_gen, epochs = 10, steps_per_epoch = 290, verbose=1, validation_data = test_gen, validation_steps = 210, callbacks = callbacks_list)
+# with tf.device('/cpu:0'):
+  # model.fit_generator(train_gen, epochs = 10, steps_per_epoch = 290, verbose=1, validation_data = test_gen, validation_steps = 210, callbacks = callbacks_list)
+
+# PPV Evaludation 
+model = load_model('models/unet_2d_1d_26.h5')
+key_lst = list(f_test['gdca'].keys())
+y_true = []
+y_pred = []
+for i in range(210):
+  print(i+1)
+  X, y = get_datapoint(f_test, key_lst[i], num_classes)
+  y_pred.append(model.predict(X))
+  y_true.append(y)
+
+one_y_true = []
+one_y_pred = []
+
+for i in y_true:
+  # ith complex (None, None, num_classes)
+  # temp = np.asarray(i)
+  # print(temp.shape)
+  for j in i:
+    for k in j:
+      for p in k:
+        # num_classes
+        one_y_true.append(np.argmax(p))
+
+# temp1 = np.asarray(y_pred)
+# temp2 = np.asarray(y_true)
+# print(temp1.shape, temp2.shape)
+for i in y_pred:
+  # ith complex (None, None, num_classes)
+  # temp = np.asarray(i)
+  # print(temp.shape)
+  for j in i:
+    for k in j:
+      for p in k:
+        # print(p)
+
+        one_y_pred.append(np.argmax(p))
+
+
+print(len(one_y_pred), len(one_y_true))
+
+confusion_matrix = confusion_matrix(one_y_true, one_y_pred)
+
+FP = confusion_matrix.sum(axis=0) - np.diag(confusion_matrix)  
+FN = confusion_matrix.sum(axis=1) - np.diag(confusion_matrix)
+TP = np.diag(confusion_matrix)
+TN = confusion_matrix.sum() - (FP + FN + TP)
+
+ACC = (TP+TN)/(TP+FP+FN+TN)
+PPV = TP/(TP+FP)
+
+print("Accuracy: ", ACC)
+print("PPV: ", PPV)
+
+'''
+num_classes = 7
+Accuracy:  [0.99791577 0.98941037 0.98323674 0.97221216 0.96044709 0.95563355
+ 0.89367793]
+PPV:  [0.98287278 0.75014834 0.62556969 0.42357862 0.52627918 0.50621767
+ 0.88414448]
+
+num_classes = 12
+Accuracy:  [0.99314271 0.99034338 0.99011985 0.98775791 0.98866754 0.98210719
+ 0.97598051 0.97912268 0.97710804 0.97602216 0.97366097 0.86859402]
+PPV:  [0.97086422 0.63947233 0.44432123 0.42859469 0.13835341 0.34136207
+ 0.33094466 0.17966295 0.2859744  0.2692568  0.18350515 0.85527541]
+
+num_classes = 26
+Accuracy:  [0.99784002 0.9986482  0.99677071 0.99398201 0.99231159 0.99421924
+ 0.99485658 0.99200519 0.99425611 0.99480463 0.99396533 0.99207605
+ 0.98921521 0.98924368 0.98633832 0.98888448 0.9900747  0.98944288
+ 0.98803498 0.98801565 0.98786755 0.98719823 0.98649811 0.98540436
+ 0.9857864  0.8372842 ]
+PPV:  [0.98327405 0.26422764 0.35851884 0.50287211 0.42459293 0.34808997
+ 0.20822712 0.35505697 0.15891505 0.1023622  0.12406217 0.08823246
+ 0.29009821 0.13604011 0.27299517 0.13679245 0.08991955 0.11254883
+ 0.24466154 0.13680602 0.16071429 0.15661765 0.12569832 0.31334832
+ 0.30307773 0.81865921]
+'''
 
 
 
